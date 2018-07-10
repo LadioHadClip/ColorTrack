2fd88c21ff86ac70baf2d6efa677c86cbffe4872
LadioHadClip <cdsadine@gmail.com>
2018-07-10 15:17:54 +0800

    Update: choose object GUI v0.1

--- code/cfChooseObject.py
+++ code/cfChooseObject.py
@@ -5,6 +5,7 @@ import cv2
 import json
 
 import bitmap
+import video_loader as vl
 
 
 # Data Structure for initial objects
@@ -60,7 +61,11 @@ class ObjectSlots(bitmap.Bitmap):
             if self.check(i):
                 if func(self._slots[i], params):
                     return i, self._slots[i]
-        return None
+        return -1, None
+
+    def get_slot(self, idx):
+        assert idx < self._size and idx >= 0 and self.check(idx), 'Slot #{} is empty.'.format(idx)
+        return self._slots[idx]
 
     def _update(self, idx, mode):
         if mode == -1:
@@ -83,69 +88,132 @@ def localize(frame, name, max_slots=10):
 
     objSlots = ObjectSlots(max_slots)
     mode = 0    # 0-draw, 1-edit
-    cur_slot = objSlots.next_slot()
-    cur_obj = [0, 0, 0, 0, False]   #[lx, ly, rx, ry, state]
+    isDown = False
+    isSelected = False
+    cur_slot = -1
+    cur_obj = [0, 0, 0, 0]   #[lx, ly, rx, ry]
+    window = cv2.namedWindow('Choose object')
 
-    def check(obj):
-        return obj.inBox(x, y)
+    def check(obj, params):
+        return obj.inBox(params['x'], params['y'])
+
+    def redrawGUI():
+        # each slots, (255, 0, 0)
+        # cur obj, (0, 255, 0)
+        nonlocal frame, objSlots, max_slots, cur_obj, isSelected
+        cur_frame = frame.copy()
+        for i in range(0, max_slots):
+            if objSlots.check(i):
+                slot = objSlots.get_slot(i)
+                cv2.rectangle(
+                    cur_frame,
+                    (int(slot.x1), int(slot.y1)),
+                    (int(slot.x2), int(slot.y2)),
+                    (255, 0, 0),
+                    2
+                )
+        if any(cur_obj):
+            _color = (0, 255, 0) if isSelected else (255, 0, 0)
+            cv2.rectangle(
+                cur_frame,
+                (int(cur_obj[0]), int(cur_obj[1])),
+                (int(cur_obj[2]), int(cur_obj[3])),
+                _color,
+                2
+            )
+        cv2.imshow('Choose object', cur_frame)
 
     def onMouse(event, x, y, flags, params):
         # callback
         # each press & release is a slot
-        pass
+        nonlocal isDown, objSlots, mode, isSelected, cur_slot, cur_obj
+        if event == cv2.EVENT_LBUTTONDOWN:
+            isDown = True
+            if mode == 0:
+                cur_slot = objSlots.next_slot()
+                cur_obj[0] = x
+                cur_obj[1] = y
+            elif mode == 1:
+                cur_slot, obj = objSlots.find_slot(check, {'x':x, 'y':y})
+                if cur_slot == -1:
+                    cur_slot = objSlots.next_slot()
+                else:
+                    isSelected = True
+                    cur_obj = [obj.x1, obj.y1, obj.x2, obj.y2]
+                    redrawGUI()
+        elif event == cv2.EVENT_LBUTTONUP:
+            isDown = False
+            if mode == 0:
+                objSlots.set_slot(
+                    InitializedObject(
+                        x1=cur_obj[0], y1=cur_obj[1],
+                        x2=cur_obj[2], y2=cur_obj[3],
+                        id=cur_slot),
+                    cur_slot
+                )
+                cur_slot = objSlots.next_slot()
+                cur_obj = [0, 0, 0, 0]
+                redrawGUI()
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if isDown and mode == 0:
+                cur_obj[2] = x
+                cur_obj[3] = y
+                redrawGUI()
   
     # main part
+    cv2.setMouseCallback('Choose object', onMouse)
+    redrawGUI()
     inProcess = True
     while(inProcess):
         key = cv2.waitKey(0)
-
+        print(key == ord('e'))
         # 'e'-edit, 'd'-delete, 's'-save, 'q'-exit
-        if key == 'e':
-            mode = 1 - mode
-            print('Now mode={}'.format(mode))
-        elif key == 'd':
-            if mode == 1:
-                # TODO: erase
-                pass
-        elif key == 's':
+        if key & 0xFF == ord('e'):
+            if not isDown:
+                mode = 1 - mode
+                isSelected = False
+                cur_obj = [0, 0, 0, 0]
+                print('Now mode={}'.format(mode))
+        elif key & 0xFF == ord('d'):
+            if mode == 1 and isSelected:
+                isSelected = False
+                objSlots.clear_slot(cur_slot)
+                cur_obj = [0, 0, 0, 0]
+                cur_slot = objSlots.next_slot()
+                redrawGUI()
+        elif key & 0xFF == ord('s'):
             # objslots -> file
-            with open(record_name, 'w') as f:
-                record = {}
-                # TODO: actual transform
-                json.dump(record, f)
-        elif key == 'q':
+            if not isDown:
+                with open(record_name, 'w') as f:
+                    record = {}
+                    # TODO: actual transform
+                    for i in range(0, max_slots):
+                        if objSlots.check(i):
+                            slot = objSlots.get_slot(i)
+                            record[str(i)] = [slot.x1, slot.y1, slot.x2, slot.y2]
+                    json.dump(record, f)
+                    print(record_name + 'Successfully saved')
+        elif key & 0xFF == ord('q'):
             inProcess = False
-
 
 # Test
 
 def main():
-    objSlots = ObjectSlots(10)
+    path = 'F:\\data\\animal\\nmice_color\\ch03_20180703185712'
+    video_name = 'ch03_20180703185712_0cropped.avi'
+    ext = '.avi'
+    # new_tar = 1
+    # new_ext = '.avi'
+    # old_ext = '.mp4'
+    # new_size = (640, 360)
 
-    try:
-        breaks = [(5, 4), (5, 2), (11, 5)]
-        cur = breaks.pop(0)
-        for i in range(20):
-            print('==== {} ===='.format(i))
-            idx = objSlots.next_slot()
-            if idx == -1:
-                break
-            A = InitializedObject(id=idx, x1=0, y1=0, x2=100, y2=100)
-            objSlots.set_slot(A, A.id)
-            print('Check: ', str(objSlots.find_slot(lambda x, y: x.inBox(y['x'], y['y']), {'x':10, 'y':10}, lru=True)))
-            print('nEXT: {}'.format(objSlots.next_slot()))
-            while i == cur[0]:
-                objSlots.clear_slot(cur[1])
-                print('Clear {}'.format(cur[1]))
-                print('nEXT: {}*'.format(objSlots.next_slot()))
-                if len(breaks) > 0: 
-                    cur = breaks.pop(0)
-                else:
-                    cur = (-1, 0)
-    except Exception as e:
-        print(e)
-    finally:
-        pass   
+    loader = vl.VideoLoader(os.path.join(path, video_name))
+    for frames, nframe in loader.get(count=16):
+        frame = frames[0]
+        localize(frame, os.path.join(path, video_name).replace(ext, ''))
+        break
+            
+  
 
 if __name__ == '__main__':
     main()
